b67309d0f77ad35acf45d702e1e49e9c98c73bfe
wenyaw <15652061178@163.com>
2018-12-11 17:25:40 +0800

    Add files via upload

--- app/src/main/java/code/com/kotlin/FmFragment.kt
+++ app/src/main/java/code/com/kotlin/FmFragment.kt
@@ -5,26 +5,24 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.frag_my_tab.*
 
 /**
- * Created by ljw on 2018/12/7.
+ * Created by ljw on 2018/12/11.
  */
 
- class MyTabFragment : Fragment() {
+ class FmFragment : Fragment() {
 
     companion object {
-        fun newInstance() : MyTabFragment{
-            return MyTabFragment()
+        fun newInstance() : FmFragment {
+            return FmFragment()
         }
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.frag_my_tab,container,false)
+        return inflater.inflate(R.layout.fragment_fm,container,false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-//        val position = arguments!!.get("position")
-//        tv_title.text = "Content: " + position
+
     }
 }
